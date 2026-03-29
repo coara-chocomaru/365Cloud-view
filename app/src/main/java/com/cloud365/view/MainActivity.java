c282d95d74f9f2abdbae2ab23fc7c4e3f8d306e6
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-03-29 14:48:15 +0900

    Update MainActivity.java

--- app/src/main/java/com/cloud365/view/MainActivity.java
+++ app/src/main/java/com/cloud365/view/MainActivity.java
@@ -102,6 +102,7 @@ public class MainActivity extends AppCompatActivity {
 
         setupWebView();
         loadMainPage();
+        broadcastStorageUpdate(storage);
     }
 
     private void setupWebView() {
